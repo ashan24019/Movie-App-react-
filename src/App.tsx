ec8b800258ba55424cc888019d2dbe20b44f3093
Ashan <ashanvimodh241@gmail.com>
2025-08-31 19:48:53 +0530

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,17 @@
-function App() {}
+import ListGroup from './components/ListGroup'
+
+function App() {
+    const items = [
+        "New York",
+        "San Francisco",
+        "Tokyo",
+        "London",
+        "Paris"
+    ];
+    const handleSelectItem = (item: string) => {
+        console.log(item);
+    }
+    return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
+}
+
+export default App
